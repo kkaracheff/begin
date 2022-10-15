1dbd6ca53f2161aed3a555772d47bcef2861586a
kkaracheff <kostya.karacheff@ukr.net>
2022-10-15 15:59:16 +0300

    Create some features

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from tkinter import CENTER
 import pygame
 from sys import exit
 
@@ -7,26 +8,47 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf',50)
 
+# Background and text init
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
+text_rect = text_surface.get_rect(center = (400,200))
 
-snail_suurface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_position = 600
+# Snail init and position 
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
+
+# Player init and position 
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print('collision')
     
+    # BACKGROUND Display
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    snail_x_position -= 4
-    if snail_x_position < -100: snail_x_position = 800
-    screen.blit(snail_suurface,(snail_x_position,250))
     
+    # SNAIL display and move
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf,snail_rect)
+    
+    # Player display and move
+    screen.blit(player_surf,player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    #     print('collision')    
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
