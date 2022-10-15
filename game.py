be6dc02aa7078d0479e09c565f5ceceb889ca3d4
krchff <kostya.karacheff@ukr.net>
2022-10-15 14:01:32 +0300

    first commit

---
+++ game.py
@@ -0,0 +1,32 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf',50)
+
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black')
+
+snail_suurface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_position = 600
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+    snail_x_position -= 4
+    if snail_x_position < -100: snail_x_position = 800
+    screen.blit(snail_suurface,(snail_x_position,250))
+    
+
+    pygame.display.update()
+    clock.tick(60)
